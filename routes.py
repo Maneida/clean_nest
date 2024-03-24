5295ef7f41641712e2a21a9de8221ba36b52308f
Michael Amuah <mkdamuah@hotmail.com>
2024-03-24 23:11:10 +0000

    updated more html templates

--- routes.py
+++ routes.py
@@ -5,10 +5,23 @@ main_routes = Blueprint('main_routes', __name__)
 
 
 @main_routes.route('/')
-def index():
+@main_routes.route('/home')
+def get_index():
     return render_template('index.html')
 
 
 @main_routes.route('/about')
-def about():
+def show_about():
     return render_template('a_about.html')
+
+@main_routes.route("/services")
+def show_services():
+    return render_template("services.html")
+
+@main_routes.route("/booking")
+def book_services():
+    return render_template("booking.html")
+
+@main_routes.route("/auth")
+def get_authentication():
+    return render_template("auth.html")
